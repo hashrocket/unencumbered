d17b392139f41603d76d68a30caed1db3150649f
Jon Larkowski and Stephen Caudill <dev+l4rk+voxdolo@hashrocket.com>
2009-11-23 12:50:59 -0800

    Rename example_group_methods to example_group

--- spec/lib/unencumbered_spec.rb
+++ spec/lib/unencumbered_spec.rb
@@ -26,42 +26,66 @@ describe Spec::Example::ExampleGroupMethods do
     include Spec::Example::ExampleGroupMethods
   end
 
-  let(:example_group_methods){ TestBed.new }
+  let(:example_group){ TestBed.new }
 
   shared_examples_for 'a standard describe proxy' do
     let(:spec_method){ spec_type.gsub(/\:/,'') }
 
     it 'calls describe' do
-      example_group_methods.should_receive(:describe)
-      example_group_methods.send(spec_method,'foo'){}
+      example_group.should_receive(:describe)
+      example_group.send(spec_method,'foo'){}
     end
 
     it "prefaces the description" do
-      example_group_methods.should_receive(:describe).with("#{spec_type} foo")
-      example_group_methods.send(spec_method,'foo'){}
+      example_group.should_receive(:describe).with("#{spec_type} foo")
+      example_group.send(spec_method,'foo'){}
     end
 
     it 'passes along the implementation block' do
       impl = lambda{ 'foo' }
-      example_group_methods.should_receive(:describe).with(anything,&impl)
-      example_group_methods.send(spec_method,'foo',&impl)
+      example_group.should_receive(:describe).with(anything,&impl)
+      example_group.send(spec_method,'foo',&impl)
     end
   end
 
   shared_examples_for 'a standard example proxy' do
     it 'calls example' do
-      example_group_methods.should_receive(:example)
-      example_group_methods.send(spec_method,'foo'){}
+      example_group.should_receive(:example)
+      example_group.send(spec_method,'foo'){}
     end
 
     it 'prefaces the description' do
-      example_group_methods.should_receive(:example).with("#{spec_method} foo")
-      example_group_methods.send(spec_method,'foo'){}
+      example_group.should_receive(:example).with("#{spec_method} foo")
+      example_group.send(spec_method,'foo'){}
     end
+
     it 'passes along the implementation block' do
       impl = lambda{ 'foo' }
-      example_group_methods.should_receive(:example).with(anything,&impl)
-      example_group_methods.send(spec_method,'foo',&impl)
+      example_group.should_receive(:example).with(anything,&impl)
+      example_group.send(spec_method,'foo',&impl)
+    end
+  end
+
+  describe '#executes' do
+    it 'calls before' do
+      example_group.should_receive(:before)
+      example_group.send(:executes){}
+    end
+
+    it 'defaults the scope to :all' do
+      example_group.should_receive(:before).with(:all)
+      example_group.send(:executes){}
+    end
+
+    it 'allows you to override the scope' do
+      example_group.should_receive(:before).with(:each)
+      example_group.send(:executes,:each){}
+    end
+
+    it 'passes along the implementation block' do
+      impl = lambda{ 'foo' }
+      example_group.should_receive(:before).with(anything,&impl)
+      example_group.send(:executes,&impl)
     end
   end
 
@@ -85,11 +109,6 @@ describe Spec::Example::ExampleGroupMethods do
     it_should_behave_like 'a standard describe proxy'
   end
 
-  describe "#Then" do
-    let(:spec_method){ 'Then' }
-    it_should_behave_like 'a standard example proxy'
-  end
-
   describe "#And" do
     let(:spec_method){ 'And' }
     it_should_behave_like 'a standard example proxy'
@@ -100,26 +119,8 @@ describe Spec::Example::ExampleGroupMethods do
     it_should_behave_like 'a standard example proxy'
   end
 
-  describe '#executes' do
-    it 'calls before' do
-      example_group_methods.should_receive(:before)
-      example_group_methods.send(:executes){}
-    end
-
-    it 'defaults the scope to :all' do
-      example_group_methods.should_receive(:before).with(:all)
-      example_group_methods.send(:executes){}
-    end
-
-    it 'allows you to override the scope' do
-      example_group_methods.should_receive(:before).with(:each)
-      example_group_methods.send(:executes,:each){}
-    end
-
-    it 'passes along the implementation block' do
-      impl = lambda{ 'foo' }
-      example_group_methods.should_receive(:before).with(anything,&impl)
-      example_group_methods.send(:executes,&impl)
-    end
+  describe "#Then" do
+    let(:spec_method){ 'Then' }
+    it_should_behave_like 'a standard example proxy'
   end
 end
